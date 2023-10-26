f7cbd4e707f3ff1ee8f21842402485bcdbfdc861
Jhonatas Paulo <jp.jhonataspaulo@gmail.com>
2023-10-26 10:21:33 -0300

    add button menu

--- src/app/dashboard/views/AppBar/index.tsx
+++ src/app/dashboard/views/AppBar/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { FC } from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 import styles from './styles.module.scss'
 import { PAGES } from '@/@core/pages'
@@ -14,7 +15,11 @@ export const AppBar: FC<Props> = () => {
   const cPage = PAGES.find(p => p.path === pathName)
   return (
     <header className={styles.header}>
+      <div className={styles.left}>
+      <GiHamburgerMenu size={24} className={styles.icon1}/>
+      <GiHamburgerMenu size={24} className={styles.icon2}/>
       <strong>{cPage?.label || 'Seja bem vindo!'}</strong>
+      </div>
       <Profile />
     </header>
   )
