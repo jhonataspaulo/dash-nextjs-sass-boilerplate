e1077e6623c24910b560b9fd8bca20548ad4639e
Jhonatas Paulo <jp.jhonataspaulo@gmail.com>
2023-10-28 13:39:51 -0300

    add page login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+
+import styles from './page.module.scss'
+
 export default function Home() {
   return (
-    <main>
-     <h1>Login</h1>
+    <main className={styles.main}>
+      <form>
+        <div className={styles.formTitle}>Login</div>
+        <div className={styles.inputs}>
+          <div className={styles.inputGroup}>
+            <label>Email</label>
+            <input type="text" placeholder='Seu email aqui'/>
+          </div>
+          <div className={styles.inputGroup}>
+            <label>Senha</label>
+            <input type="password" placeholder='Sua senha aqui'/>
+          </div>
+        </div>
+        <button>Entrar</button>
+      </form>
     </main>
   )
 }
+
